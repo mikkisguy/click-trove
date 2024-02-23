2ea0d66990c090ac5a4f615bc2720878caa5329f
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2024-02-23 21:26:27 +0200

    Removed now obsolete stylings

--- app/shared/types/configs.d.ts
+++ app/shared/types/configs.d.ts
@@ -1,23 +1,7 @@
 /**
- * Represents the configuration keys.
- */
-export type ConfigKey = "GENERAL" | "COLORS";
-
-/**
  * Represents general configurations for the game.
  */
 export type GeneralConfigs = {
   gameTitle: string; // The title of the game
   gameDescription: string; // The description of the game
 };
-
-/**
- * Represents color configurations for the game.
- */
-export type ColorConfigs = {
-  background: {
-    main: string; // The main background color
-  };
-};
-
-export type Configs = GeneralConfigs | ColorConfigs;
